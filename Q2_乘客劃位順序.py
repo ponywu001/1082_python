174f17778bc971cf06a8685a67543aa1c57022ed
ponywu001 <64211281+ponywu001@users.noreply.github.com>
2020-05-21 16:01:10 +0800

    Update Q2_乘客劃位順序.py

--- Q2_乘客劃位順序.py
+++ Q2_乘客劃位順序.py
@@ -8,3 +8,15 @@ Created on Tue Apr 28 21:14:31 2020
 """
 進階程式設計 HW-2
 """
+
+a = []
+
+for i in range(eval(input())):
+    s = [x for x in input().split(" ")]
+    p = int(s[1]) / 1000 + int(s[2]) - int(s[3])
+    a.append((s[0], int(p)))
+
+a.sort(key=(lambda x:x[1]))
+
+for i in range(len(a)):
+    print(a[i][0], a[i][1])
